eb6da636881ef083a1852fa406df359c1c9eb1c3
SantiagoPineda0353 <Santiagopineda0353@gmail.com>
2026-08-07 14:36:13 -0500

    Inicio de proyecto

---
+++ src/main/java/com/webFlux/crudWebFlux/CrudWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.webFlux.crudWebFlux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudWebFluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webFlux/crudWebFlux/CrudWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webFlux.crudWebFlux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
